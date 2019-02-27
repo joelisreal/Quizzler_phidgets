d633990d742336e95103081b830f56d2d09edd0d
joelisreal <joel.igberase@gmail.com>
2019-02-27 12:54:52 -0700

    Added alert functionality as well as lets the user know if they got a question right or not.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
+    var score : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +24,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        questionNumber += 1
+       
+        nextQuestion()
+        
+        
     }
     
     
@@ -36,16 +54,38 @@ class ViewController: UIViewController {
 
     func nextQuestion() {
         
+        if questionNumber <= 12 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        }
+        else {
+            let alert = UIAlertController(title: "End of Quiz", message: "Do you want to start over", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertAction) in self.startOver()
+                })
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil )
+        }
     }
     
     
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("You got it!")
+        }
+        else {
+            print("wrong!")
+        }
     }
     
     
     func startOver() {
-       
+       questionNumber = 0
+        nextQuestion()
     }
     
 
